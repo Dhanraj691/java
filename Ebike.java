ba79f664c99a447969e701b489cb812acaeb10ac
Dhanraj691 <rajdhanra@gmail.com>
2021-09-04 09:45:42 +0530

    updated Ebike.java

---
+++ Ebike.java
@@ -0,0 +1,20 @@
+public class Ebike                             //Ebike - class name
+{
+
+public static void main(String args[])
+	{
+	
+	Ebike rider =new Ebike();                 //new Ebike() - is an Object, rider is a Object name
+	
+	rider.putStartkey();                      //Calling Method using Object
+	
+	}
+	
+void putStartkey()                            //Method creation 
+	{
+	
+	System.out.println("Ready to ride..");
+	
+	}
+	
+}
