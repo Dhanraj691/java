13039426aaf0592ffe5fe984fe89f98d9921977d
Dhanraj691 <rajdhanra@gmail.com>
2021-10-09 23:57:21 +0530

    The sum of any three consecutive terms of the series gives the next term

---
+++ loopingexamples/SumofThree_terms.java
@@ -0,0 +1,40 @@
+package loopingexamples;
+
+public class SumofThree_terms {
+
+	public static void main(String[] args) 
+	{
+		int i=1;
+		int j=3;
+		int k=4;
+		int sum=0;
+		
+		System.out.println(i+"\n"+j+"\n"+k);
+		
+		while(sum<50)
+		{
+			sum=i+j+k;
+			i=j;
+			j=k;
+			k=sum;
+			System.out.println(sum);
+		}
+		
+	}
+
+}
+
+/* Output:
+ 
+1
+3
+4
+8
+15
+27
+50
+
+*/
+ 
+
+
