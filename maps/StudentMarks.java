c5ac888385f3aed762f8dfd20a44665a5f092d7d
Dhanraj691 <rajdhanra@gmail.com>
2022-05-17 23:55:30 +0530

    collections

---
+++ maps/StudentMarks.java
@@ -0,0 +1,33 @@
+package maps;
+import java.util.*;
+
+public class StudentMarks extends Exception  {
+
+	public static void main(String[] args) {
+		HashMap hm=new HashMap();
+		hm.put("Dhanraj",360);
+		hm.put("Raj",369);
+		hm.put("Dhana",407);
+		hm.put("Eliz",360);
+		hm.put("Ravi",290);
+		
+		Set s=hm.entrySet();
+		Iterator i=s.iterator();
+		boolean b=i.hasNext();
+		try {
+		while(b=true)
+		{
+			Object o=i.next();
+			Map.Entry entry=(Map.Entry)o;
+			System.out.println(entry.getKey()+" get "+entry.getValue()+" marks");
+			b=i.hasNext();
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Map is end...");
+		}
+
+	}
+
+}
